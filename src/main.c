7206186596e872acd29069831c684c57b964e87b
Grant Horner <h.grant.horner@gmail.com>
2020-12-18 21:39:39 -0500

    Can add "stones"

--- src/main.c
+++ src/main.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <math.h>
+#include <stdbool.h>
 #include "consts.h"
 
-typedef struct Coordinate {
+typedef struct {
   int x;
   int y;
 } Coordinate;
+
+typedef enum {Black, White} Color;
+
+typedef struct {
+  Coordinate coord;
+  Color color;
+  int intersection_index;
+  bool destroyed;
+} Stone;
+
+typedef struct {
+  Stone stones[1000];
+  int next_index;
+  Color turn;
+} Stones;
 
 int init_SDL(SDL_Window **window,
              SDL_Renderer **renderer) {
@@ -49,7 +65,7 @@ void draw_square(SDL_Renderer *renderer,
   }
 }
 
-void draw_board(SDL_Renderer *renderer, Coordinate *clicked) {
+void draw_board(SDL_Renderer *renderer, const Stones *stones) {
   SDL_SetRenderDrawColor(renderer, 166, 104, 41, 255);
   SDL_RenderClear(renderer);
 
@@ -70,10 +86,15 @@ void draw_board(SDL_Renderer *renderer, Coordinate *clicked) {
                        SCREEN_WIDTH - PADDING + 1);    
   }
 
-  if (clicked != NULL) {
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    const SDL_Rect rect = { clicked->x - 3, clicked->y - 3, 6, 6 };
-    SDL_RenderFillRect(renderer, &rect);
+  if (stones != NULL) {
+    for (int i = 0; i < stones->next_index; i++) {
+      const Stone stone = stones->stones[i];
+      if (stone.color == Black) SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+      if (stone.color == White) SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+      const SDL_Rect rect = { stone.coord.x - 3, stone.coord.y - 3, 6, 6 };
+      SDL_RenderFillRect(renderer, &rect);
+    }
   }
 }
 
@@ -100,29 +121,30 @@ void draw_intersections(SDL_Renderer *renderer, const Coordinate coords[]) {
 
 // index of array = i * 19 + j
 // x = square_len * i + padding -> x = 24 * i + 30
-// index of array = ((x - 30) / 24) * 19 + j
 // y = square_len * j + padding -> y = 24 * j + 30
 // index of array = ((x - 30) / 24) * 19 + (y - 30) / 24
-Coordinate *find_nearest_intersection(Coordinate coords[], const Coordinate click) {
+void place_stone(Coordinate coords[], const Coordinate click, Stones *stones) {
   const int i = round((click.x - PADDING) / (float) SQUARE_LENGTH);
   const int j = round((click.y - PADDING) / (float) SQUARE_LENGTH);
   const int index = i * 19 + j;
-  Coordinate *clicked = &coords[index];
-  printf("%d, %d vs %d, %d\n", clicked->x, clicked->y, click.x, click.y);
-  // printf("%d: %d, %d\n", index, coords[index].x, coords[index].y);
-  // printf("X: %d, Y: %d\n", click.x, click.y);
-  return clicked;
+  Coordinate clicked = coords[index];
+
+  const Stone stone = { clicked, stones->turn, index, false };
+  stones->stones[stones->next_index] = stone;
+  if (stones->turn == Black) stones->turn = White;
+  else if (stones->turn == White) stones->turn = Black;
+  stones->next_index++;  
 }
 
-void handle_inputs(int *running, SDL_Renderer *renderer, Coordinate coordinates[], Coordinate **clicked_intersection) {
+void handle_inputs(bool *running, SDL_Renderer *renderer, Coordinate coordinates[], Stones *stones) {
   SDL_Event event;
   if (SDL_PollEvent(&event)) {
     if (event.type == SDL_MOUSEBUTTONUP) {
        const Coordinate click = { event.button.x, event.button.y};
-       *clicked_intersection = find_nearest_intersection(coordinates, click);
+       place_stone(coordinates, click, stones);
     }
     if (event.type == SDL_QUIT) {
-      *running = 0;
+      *running = false;
     }
   }  
 }
@@ -133,15 +155,15 @@ int main(void) {
 
   if (init_SDL(&window, &renderer) < 0) return -1;
   
-  int running = 1;
+  bool running = true;
 
   const int render_timer = roundf(1000.0f / (float) FPS);
 
   Coordinate coordinates[NUM_INTERSECTIONS];
-
   generate_coordinates(coordinates);
   
-  Coordinate *clicked_intersection = NULL;
+  Stones stones = { {}, 0, Black };
+  
   while (running) {
     const int start_frame_time = SDL_GetTicks();
     
@@ -153,10 +175,10 @@ int main(void) {
       printf("Failed to clear the render. SDL Error: %s\n", SDL_GetError());
     }
 
-    draw_board(renderer, clicked_intersection);
+    draw_board(renderer, &stones);
     //draw_intersections(renderer, coordinates);
 
-    handle_inputs(&running, renderer, coordinates, &clicked_intersection);
+    handle_inputs(&running, renderer, coordinates, &stones);
 
     const int end_frame_time = SDL_GetTicks();
     const int delta_time = end_frame_time - start_frame_time;
